5a919d7812e8d78e8d5442a530155dbc1cc4a9cd
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2025-07-17 13:18:26 +0100

    feat: working on the client page

---
+++ src/app/client/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ClientImage from '@/app/assets/png/client-image.png'
+import ClientImage2 from '@/app/assets/png/client-image2.png'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import ClientWork from '../features/homepage/client-works'
+import ClientMessage from '../features/client/client-message'
+
+function Client () {
+  return (
+    <section className='py-26'>
+      <main className='flex items-start justify-between mt-10'>
+        <Image src={ClientImage} alt='client image' height={600} />
+        <div className='flex flex-col items-center justify-center mt-10'>
+          <h1 className='text-[70px] font-normal text-center leading-tight'>
+            Collaborations <br /> Rooted in Creativity
+          </h1>
+          <p className='text-center font-satoshi text-[#4E5157]'>
+            From J.H Textile studios to large-scale spaces, we bring textile
+            ideas to life.
+          </p>
+          <Button className='bg-black rounded-none flex justify-center items-center mt-4 h-10'>
+            View Collections
+          </Button>
+        </div>
+        <Image src={ClientImage2} alt='client image' height={600} />
+      </main>
+      <ClientWork />
+      <ClientMessage />
+    </section>
+  )
+}
+
+export default Client
---
+++ src/app/features/client/client-message.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Newsletter from '@public/assets/png/newsletter.png'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+function ClientMessage () {
+  return (
+    <section className='relative min-h-[60vh] flex items-center justify-center mt-[50px]'>
+      <Image
+        src={Newsletter}
+        alt='Colorful traditional textiles and rugs background'
+        fill
+        className='object-cover'
+        priority
+      />
+      <div className='relative z-10 text-center max-w-2xl mx-auto px-6'>
+        <h2 className='text-6xl lg:text-5xl font-light text-white mb-4 tracking-wide'>
+          Have a textile vision? Let’s bring it to life
+        </h2>
+
+        <p className='text-white/90 text-lg mb-4 font-light font-satoshi md:text-nowrap'>
+          We take on custom work, collaborative design projects, and large-scale commissions.
+        </p>
+        <Button className='text-black bg-white rounded-none'>
+            Start a conversation
+        </Button>
+      </div>
+    </section>
+  )
+}
+
+export default ClientMessage
