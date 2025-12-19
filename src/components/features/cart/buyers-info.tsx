8ae5edad82c60bd71113037da3c1cab24022870d
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2025-12-19 22:32:10 +0100

    fix: removed delivery details

--- src/components/features/cart/buyers-info.tsx
+++ src/components/features/cart/buyers-info.tsx
@@ -10,7 +10,7 @@ import { useCurrency } from '@/context/currency-context'
 import { useCheckout } from '@/context/checkout-context'
 import { buyersInfoSchema, BuyersInfoFormData } from '@/validators/checkout-validators'
 
-function BuyersInfo ({
+function BuyersInfo({
   handleNext,
   handlePrevious
 }: {
@@ -20,24 +20,19 @@ function BuyersInfo ({
   const { cart, getCartTotal } = useCart()
   const { formatPrice } = useCurrency()
   const { checkoutData, updateCheckoutData } = useCheckout()
-  
+
   const methods = useForm<BuyersInfoFormData>({
     resolver: zodResolver(buyersInfoSchema),
     defaultValues: {
       first_name: checkoutData.firstName || '',
       last_name: checkoutData.lastName || '',
       email: checkoutData.email || '',
-      phone_number: checkoutData.phoneNumber || '',
-      delivery_country: checkoutData.deliveryCountry || '',
-      delivery_state: checkoutData.deliveryState || '',
-      delivery_city: checkoutData.deliveryCity || '',
-      delivery_postal_code: checkoutData.deliveryPostalCode || '',
-      delivery_address: checkoutData.deliveryAddress || ''
+      phone_number: checkoutData.phoneNumber || ''
     }
   })
 
   const subtotal = getCartTotal()
-  const shipping = 0 
+  const shipping = 0
   const total = subtotal + shipping
 
   const onSubmit = (data: BuyersInfoFormData) => {
@@ -45,172 +40,79 @@ function BuyersInfo ({
       firstName: data.first_name,
       lastName: data.last_name,
       email: data.email,
-      phoneNumber: data.phone_number,
-      deliveryCountry: data.delivery_country,
-      deliveryState: data.delivery_state,
-      deliveryCity: data.delivery_city,
-      deliveryPostalCode: data.delivery_postal_code,
-      deliveryAddress: data.delivery_address,
+      phoneNumber: data.phone_number
     })
-    
+
     handleNext()
   }
 
   return (
-    <section className='flex md:flex-row flex-col gap-4 items-start mt-10 md:mt-20'>
-      <div className='md:w-[60%] w-full md:border-r border-black md:pr-6'>
+    <section className="flex md:flex-row flex-col gap-4 items-start mt-10 md:mt-20">
+      {/* Form */}
+      <div className="md:w-[60%] w-full md:border-r border-black md:pr-6">
         <FormProvider {...methods}>
-          <section className='md:p-6 pt-0 w-full'>
+          <section className="md:p-6 pt-0 w-full">
             <form
               onSubmit={methods.handleSubmit(onSubmit)}
-              className='flex flex-col justify-center items-start mt-[10px] gap-6'
+              className="flex flex-col justify-center items-start mt-[10px] gap-6"
             >
-              <p className='font-satoshi text-black text-lg md:text-[24px]'>
-                Delivery Address
+              <p className="font-satoshi text-black text-lg md:text-[24px]">
+                Buyer Information
               </p>
-              
-              <div className='w-full flex flex-col gap-4'>
-                <div className='flex md:flex-row flex-col gap-3 items-center'>
-                  <div className='w-full'>
+
+              <div className="w-full flex flex-col gap-4">
+                <div className="flex md:flex-row flex-col gap-3 items-center">
+                  <div className="w-full">
                     <FormInput
-                      name='first_name'
-                      type='text'
-                      placeholder='Enter your first name'
-                      className='h-[52px]'
+                      name="first_name"
+                      type="text"
+                      placeholder="Enter your first name"
+                      className="h-[52px]"
                     />
-                    {/* {methods.formState.errors.first_name && (
-                      <p className='text-red-500 text-sm mt-1'>
-                        {methods.formState.errors.first_name.message}
-                      </p>
-                    )} */}
                   </div>
-                  <div className='w-full'>
+
+                  <div className="w-full">
                     <FormInput
-                      name='last_name'
-                      type='text'
-                      placeholder='Enter your last name'
-                      className='h-[52px]'
+                      name="last_name"
+                      type="text"
+                      placeholder="Enter your last name"
+                      className="h-[52px]"
                     />
-                    {/* {methods.formState.errors.last_name && (
-                      <p className='text-red-500 text-sm mt-1'>
-                        {methods.formState.errors.last_name.message}
-                      </p>
-                    )} */}
                   </div>
                 </div>
 
-                <div className='w-full'>
+                <div className="w-full">
                   <FormInput
-                    name='email'
-                    type='email'
-                    placeholder='Enter your email'
-                    className='h-[52px]'
+                    name="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    className="h-[52px]"
                   />
-                  {/* {methods.formState.errors.email && (
-                    <p className='text-red-500 text-sm mt-1'>
-                      {methods.formState.errors.email.message}
-                    </p>
-                  )} */}
                 </div>
 
-                <div className='w-full'>
+                <div className="w-full">
                   <FormInput
-                    name='phone_number'
-                    type='tel'
-                    placeholder='Enter your phone number'
-                    className='h-[52px]'
+                    name="phone_number"
+                    type="tel"
+                    placeholder="Enter your phone number"
+                    className="h-[52px]"
                   />
-                  {/* {methods.formState.errors.phone_number && (
-                    <p className='text-red-500 text-sm mt-1'>
-                      {methods.formState.errors.phone_number.message}
-                    </p>
-                  )} */}
-                </div>
-
-                <div className='flex md:flex-row flex-col gap-3 items-center'>
-                  <div className='w-full'>
-                    <FormInput
-                      name='delivery_country'
-                      type='text'
-                      placeholder='Enter your country'
-                      className='h-[52px]'
-                    />
-                    {/* {methods.formState.errors.delivery_country && (
-                      <p className='text-red-500 text-sm mt-1'>
-                        {methods.formState.errors.delivery_country.message}
-                      </p>
-                    )} */}
-                  </div>
-                  <div className='w-full'>
-                    <FormInput
-                      name='delivery_state'
-                      type='text'
-                      placeholder='Enter your state'
-                      className='h-[52px]'
-                    />
-                    {/* {methods.formState.errors.delivery_state && (
-                      <p className='text-red-500 text-sm mt-1'>
-                        {methods.formState.errors.delivery_state.message}
-                      </p>
-                    )} */}
-                  </div>
-                </div>
-
-                <div className='flex md:flex-row flex-col gap-3 items-center'>
-                  <div className='w-full'>
-                    <FormInput
-                      name='delivery_city'
-                      type='text'
-                      placeholder='Enter your city'
-                      className='h-[52px]'
-                    />
-                    {/* {methods.formState.errors.delivery_city && (
-                      <p className='text-red-500 text-sm mt-1'>
-                        {methods.formState.errors.delivery_city.message}
-                      </p>
-                    )} */}
-                  </div>
-                  <div className='w-full'>
-                    <FormInput
-                      name='delivery_postal_code'
-                      type='text'
-                      placeholder='Enter your postal code'
-                      className='h-[52px]'
-                    />
-                    {/* {methods.formState.errors.delivery_postal_code && (
-                      <p className='text-red-500 text-sm mt-1'>
-                        {methods.formState.errors.delivery_postal_code.message}
-                      </p>
-                    )} */}
-                  </div>
-                </div>
-
-                <div className='w-full'>
-                  <FormInput
-                    name='delivery_address'
-                    type='text'
-                    placeholder='Enter your home address'
-                    className='h-[52px]'
-                  />
-                  {/* {methods.formState.errors.delivery_address && (
-                    <p className='text-red-500 text-sm mt-1'>
-                      {methods.formState.errors.delivery_address.message}
-                    </p>
-                  )} */}
                 </div>
               </div>
 
-              <div className='md:flex hidden gap-4 items-center w-full'>
+              {/* Desktop buttons */}
+              <div className="md:flex hidden gap-4 items-center w-full">
                 <button
-                  type='button'
-                  className='mt-4 bg-white text-black border border-black h-16 px-6 py-3 text-sm w-full rounded-none font-satoshi font-normal'
+                  type="button"
+                  className="mt-4 bg-white text-black border border-black h-16 px-6 py-3 text-sm w-full rounded-none font-satoshi font-normal"
                   onClick={handlePrevious}
                 >
                   Previous
                 </button>
+
                 <button
-                  type='submit'
-                  className='mt-4 bg-black text-white px-6 py-3 h-16 text-sm w-full rounded-none font-satoshi font-normal'
+                  type="submit"
+                  className="mt-4 bg-black text-white px-6 py-3 h-16 text-sm w-full rounded-none font-satoshi font-normal"
                 >
                   Next
                 </button>
@@ -219,16 +121,16 @@ function BuyersInfo ({
           </section>
         </FormProvider>
       </div>
-      
-      {/* Order Summary remains the same */}
-      <section className='bg-[#E8E7D7] md:p-8 p-4 pt-8 mx-auto w-full md:w-[40%] md:mt-0 mt-5'>
-        <h2 className='font-bold text-[#1C1B0B] text-[24px] text-center pb-6 uppercase'>
+
+      {/* Order Summary */}
+      <section className="bg-[#E8E7D7] md:p-8 p-4 pt-8 mx-auto w-full md:w-[40%] md:mt-0 mt-5">
+        <h2 className="font-bold text-[#1C1B0B] text-[24px] text-center pb-6 uppercase">
           Order Summary
         </h2>
 
-        <div className='space-y-2 max-h-[400px] overflow-y-auto'>
+        <div className="space-y-2 max-h-[400px] overflow-y-auto">
           {cart.map((item) => (
-            <OrderSummaryCard 
+            <OrderSummaryCard
               key={item.id}
               title={item.title}
               price={item.price}
@@ -241,34 +143,40 @@ function BuyersInfo ({
             />
           ))}
         </div>
-        <div className='py-8 flex flex-col gap-3 font-satoshi'>
-          <div className='flex justify-between text-[20px] font-medium text-[#1C1B0B]'>
-            <p className='font-light'>Subtotal</p>
+
+        <div className="py-8 flex flex-col gap-3 font-satoshi">
+          <div className="flex justify-between text-[20px] font-medium text-[#1C1B0B]">
+            <p className="font-light">Subtotal</p>
             <p>{formatPrice(subtotal)}</p>
           </div>
-          <div className='flex justify-between text-[20px] font-medium text-[#1C1B0B]'>
-            <p className='font-light'>Shipping</p>
+
+          <div className="flex justify-between text-[20px] font-medium text-[#1C1B0B]">
+            <p className="font-light">Shipping</p>
             <p>{shipping === 0 ? 'FREE' : formatPrice(shipping)}</p>
           </div>
         </div>
-        <div className='bg-gray-400 w-full h-px' />
-        <div className='flex justify-between pt-4 pb-4 text-[20px] font-medium text-[#1C1B0B]'>
+
+        <div className="bg-gray-400 w-full h-px" />
+
+        <div className="flex justify-between pt-4 pb-4 text-[20px] font-medium text-[#1C1B0B]">
           <p>Total</p>
           <p>{formatPrice(total)}</p>
         </div>
       </section>
 
-      <div className='md:hidden flex flex-col gap-4 items-center w-full'>
+      {/* Mobile buttons */}
+      <div className="md:hidden flex flex-col gap-4 items-center w-full">
         <button
-          type='button'
-          className='mt-4 bg-white text-black border border-black h-13 px-6 py-3 text-sm w-full rounded-none font-satoshi font-normal'
+          type="button"
+          className="mt-4 bg-white text-black border border-black h-13 px-6 py-3 text-sm w-full rounded-none font-satoshi font-normal"
           onClick={handlePrevious}
         >
           Previous
         </button>
+
         <button
-          type='submit'
-          className='mt-4 bg-black text-white px-6 py-3 h-13 text-sm w-full rounded-none font-satoshi font-normal'
+          type="submit"
+          className="mt-4 bg-black text-white px-6 py-3 h-13 text-sm w-full rounded-none font-satoshi font-normal"
           onClick={methods.handleSubmit(onSubmit)}
         >
           Next
--- src/validators/checkout-validators.ts
+++ src/validators/checkout-validators.ts
@@ -1,156 +1,135 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const buyersInfoSchema = z.object({
   first_name: z
     .string()
-    .min(2, 'First name must be at least 2 characters')
-    .max(50, 'First name must be less than 50 characters')
-    .regex(/^[a-zA-Z\s'-]+$/, 'First name can only contain letters, spaces, hyphens, and apostrophes'),
-  
+    .min(2, "First name must be at least 2 characters")
+    .max(50, "First name must be less than 50 characters")
+    .regex(
+      /^[a-zA-Z\s'-]+$/,
+      "First name can only contain letters, spaces, hyphens, and apostrophes"
+    ),
+
   last_name: z
     .string()
-    .min(2, 'Last name must be at least 2 characters')
-    .max(50, 'Last name must be less than 50 characters')
-    .regex(/^[a-zA-Z\s'-]+$/, 'Last name can only contain letters, spaces, hyphens, and apostrophes'),
-  
-  email: z
-    .string()
-    .email('Please enter a valid email address'),
-  
+    .min(2, "Last name must be at least 2 characters")
+    .max(50, "Last name must be less than 50 characters")
+    .regex(
+      /^[a-zA-Z\s'-]+$/,
+      "Last name can only contain letters, spaces, hyphens, and apostrophes"
+    ),
+
+  email: z.string().email("Please enter a valid email address"),
+
   phone_number: z
     .string()
-    .regex(/^[+]?[(]?[0-9]{1,4}[)]?[-\s.]?[(]?[0-9]{1,4}[)]?[-\s.]?[0-9]{1,9}$/, 
-      'Please enter a valid phone number'),
-  
-  delivery_country: z
-    .string()
-    .min(2, 'Country is required'),
-  
-  delivery_state: z
-    .string()
-    .min(2, 'State/Province is required'),
-  
-  delivery_city: z
-    .string()
-    .min(2, 'City is required'),
-  
-  delivery_postal_code: z
-    .string()
-    .min(3, 'Postal code must be at least 3 characters')
-    .max(20, 'Postal code is too long'),
-  
-  delivery_address: z
-    .string()
-    .min(5, 'Address must be at least 5 characters')
-    .max(100, 'Address must be less than 100 characters')
-})
+    .regex(
+      /^[+]?[(]?[0-9]{1,4}[)]?[-\s.]?[(]?[0-9]{1,4}[)]?[-\s.]?[0-9]{1,9}$/,
+      "Please enter a valid phone number"
+    ),
+});
 
-export type BuyersInfoFormData = z.infer<typeof buyersInfoSchema>
+export type BuyersInfoFormData = z.infer<typeof buyersInfoSchema>;
 
 // Card Details Validation
 export const cardDetailsSchema = z.object({
   card_number: z
     .string()
-    .regex(/^\d{13,19}$/, 'Card number must be between 13 and 19 digits')
-    .refine((val) => luhnCheck(val), 'Invalid card number'),
-  
+    .regex(/^\d{13,19}$/, "Card number must be between 13 and 19 digits")
+    .refine((val) => luhnCheck(val), "Invalid card number"),
+
   card_holder: z
     .string()
-    .min(3, 'Cardholder name must be at least 3 characters')
-    .max(50, 'Cardholder name must be less than 50 characters')
-    .regex(/^[a-zA-Z\s'-]+$/, 'Cardholder name can only contain letters, spaces, hyphens, and apostrophes'),
-  
+    .min(3, "Cardholder name must be at least 3 characters")
+    .max(50, "Cardholder name must be less than 50 characters")
+    .regex(
+      /^[a-zA-Z\s'-]+$/,
+      "Cardholder name can only contain letters, spaces, hyphens, and apostrophes"
+    ),
+
   expiry_date: z
     .string()
-    .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, 'Expiry date must be in MM/YY format')
-    .refine((val) => isValidExpiryDate(val), 'Card has expired'),
-  
-  cvv: z
-    .string()
-    .regex(/^\d{3,4}$/, 'CVV must be 3 or 4 digits'),
-  
-  billing_country: z
-    .string()
-    .min(2, 'Country is required'),
-  
-  billing_state: z
-    .string()
-    .min(2, 'State/Province is required'),
-  
-  billing_city: z
-    .string()
-    .min(2, 'City is required'),
-  
+    .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, "Expiry date must be in MM/YY format")
+    .refine((val) => isValidExpiryDate(val), "Card has expired"),
+
+  cvv: z.string().regex(/^\d{3,4}$/, "CVV must be 3 or 4 digits"),
+
+  billing_country: z.string().min(2, "Country is required"),
+
+  billing_state: z.string().min(2, "State/Province is required"),
+
+  billing_city: z.string().min(2, "City is required"),
+
   billing_postal_code: z
     .string()
-    .min(3, 'Postal code must be at least 3 characters')
-    .max(20, 'Postal code is too long'),
-  
+    .min(3, "Postal code must be at least 3 characters")
+    .max(20, "Postal code is too long"),
+
   billing_address: z
     .string()
-    .min(5, 'Address must be at least 5 characters')
-    .max(100, 'Address must be less than 100 characters')
-})
+    .min(5, "Address must be at least 5 characters")
+    .max(100, "Address must be less than 100 characters"),
+});
 
-export type CardDetailsFormData = z.infer<typeof cardDetailsSchema>
+export type CardDetailsFormData = z.infer<typeof cardDetailsSchema>;
 
 // Helper function: Luhn Algorithm for card number validation
 export function luhnCheck(cardNumber: string): boolean {
-  const digits = cardNumber.replace(/\D/g, '')
-  let sum = 0
-  let isEven = false
+  const digits = cardNumber.replace(/\D/g, "");
+  let sum = 0;
+  let isEven = false;
 
   for (let i = digits.length - 1; i >= 0; i--) {
-    let digit = parseInt(digits[i], 10)
+    let digit = parseInt(digits[i], 10);
 
     if (isEven) {
-      digit *= 2
+      digit *= 2;
       if (digit > 9) {
-        digit -= 9
+        digit -= 9;
       }
     }
 
-    sum += digit
-    isEven = !isEven
+    sum += digit;
+    isEven = !isEven;
   }
 
-  return sum % 10 === 0
+  return sum % 10 === 0;
 }
 
 // Helper function: Check if expiry date is valid
 export function isValidExpiryDate(expiryDate: string): boolean {
-  const [month, year] = expiryDate.split('/')
-  const currentDate = new Date()
-  const currentYear = currentDate.getFullYear() % 100
-  const currentMonth = currentDate.getMonth() + 1
+  const [month, year] = expiryDate.split("/");
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear() % 100;
+  const currentMonth = currentDate.getMonth() + 1;
 
-  const expYear = parseInt(year, 10)
-  const expMonth = parseInt(month, 10)
+  const expYear = parseInt(year, 10);
+  const expMonth = parseInt(month, 10);
 
   if (expYear > currentYear) {
-    return true
+    return true;
   }
 
   if (expYear === currentYear) {
-    return expMonth >= currentMonth
+    return expMonth >= currentMonth;
   }
 
-  return false
+  return false;
 }
 
 // Helper function: Format card number input (add spaces every 4 digits)
 export function formatCardNumber(value: string): string {
   return value
-    .replace(/\s/g, '')
-    .replace(/(\d{4})/g, '$1 ')
-    .trim()
+    .replace(/\s/g, "")
+    .replace(/(\d{4})/g, "$1 ")
+    .trim();
 }
 
 // Helper function: Format expiry date input (auto-add slash)
 export function formatExpiryDate(value: string): string {
-  const cleaned = value.replace(/\D/g, '')
+  const cleaned = value.replace(/\D/g, "");
   if (cleaned.length >= 2) {
-    return `${cleaned.slice(0, 2)}/${cleaned.slice(2, 4)}`
+    return `${cleaned.slice(0, 2)}/${cleaned.slice(2, 4)}`;
   }
-  return cleaned
+  return cleaned;
 }
