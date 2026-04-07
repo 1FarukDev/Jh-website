1db9d34285a3d7eb0feaa5086c1a3dff3595994d
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2026-04-07 22:13:34 +0100

    updating

--- next.config.ts
+++ next.config.ts
@@ -7,16 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['www.pexels.com', 'images.pexels.com','res.cloudinary.com', 'via.placeholder.com', 'nrzyuthynauicapsfgdz.supabase.co']
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'jh-website-lime.vercel.app' }],
-        destination: 'https://jesudarahinmikaiye.com/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 export default nextConfig
